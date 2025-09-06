85293de1957c21e475cb4264aa9f1a2412838240
Minhyeok Park <pmh_only@pmh.codes>
2025-09-06 11:19:11 +0900

    feat: change to automation v2. more robust using json output system

--- automation.mjs
+++ automation.mjs
@@ -1,13 +1,8 @@
-import fs from 'fs'
+import fs from 'fs/promises'
 import OpenAI from 'openai'
 import { Client } from 'discord.js'
-
-const original_source = fs.readFileSync('./workspace/main.mjs').toString('utf-8')
-
-const changelog_prompt = fs.readFileSync('./proms/changelog_prompt.txt').toString('utf-8')
-const commit_prompt = fs.readFileSync('./proms/commit_prompt.txt').toString('utf-8')
-const package_prompt = fs.readFileSync('./proms/package_prompt.txt').toString('utf-8')
-const source_prompt = fs.readFileSync('./proms/source_prompt.txt').toString('utf-8').replace('{{source}}', original_source)
+import { zodTextFormat } from 'openai/helpers/zod.js'
+import z from 'zod'
 
 const client = new OpenAI()
 const discord = new Client({
@@ -20,117 +15,78 @@ const changelog = await discord.channels.fetch(process.env.DISCORD_CHANNEL_ID)
 
 await changelog.send(new Date() + ' start creating new version')
 
-const source_response = await client.responses.create({
-  model: 'gpt-5-mini',
+const sourceFiles = await fs.readdir('./workspace', { recursive: true })
+const sourceFileJobs = sourceFiles.map((sourceFile) => 
+  new Promise(async (resolve) => {
+    const data = await fs.readFile('./workspace/' + sourceFile, 'utf-8')
+    resolve({
+      data,
+      fileName: sourceFile
+    })
+  })
+)
+
+const sourceFileContents = await Promise.all(sourceFileJobs)
+const taskFiles = await fs.readdir('./prompts/tasks', { recursive: true })
+const taskFileContents = await Promise.all(taskFiles.map((v) => fs.readFile('./prompts/tasks/' + v, 'utf-8')))
+
+const promptRaw = await fs.readFile('./prompts/main.txt', 'utf-8')
+const prompt = promptRaw
+  .replace('{{FILES}}', JSON.stringify(sourceFileContents, null, 2))
+  .replace('{{TASK}}', taskFileContents[Math.floor(Math.random() * taskFileContents.length)])
+
+const response = await client.responses.parse({
+  model: 'gpt-5',
+  store: true,
   input: [{
     role: 'user',
-    content: source_prompt
+    content: prompt
   }],
-  max_output_tokens: 16384
-})
-
-await changelog.send(new Date() + ' finished: ' + source_response.output_text.length + ' characters')
-const changes = source_response.output_text.split('<<<<<<< ORIGINAL').slice(1).map((v) => v.split('=======').map((v) => v.trim().replace('>>>>>>> UPDATED', '')))
-
-let source = original_source
-
-for (const [original, updated] of changes) 
-  source = source.replace(original, updated)
-
-fs.writeFileSync('./workspace/main.mjs', source)
-
-const package_response = await client.responses.create({
-  model: 'gpt-5-mini',
-  reasoning: { effort: "high" },
-  text: { verbosity: "high" },
-  input: [
-    {
-      role: 'user',
-      content: source_prompt
-    },
-    {
-      role: 'assistant',
-      content: source_response.output_text
-    },
-    {
-      role: 'user',
-      content: package_prompt
-    },
-  ]
-})
-
-fs.writeFileSync('./workspace/package.json', package_response.output_text)
-
-const changelog_response = await client.responses.create({
-  model: 'gpt-5-nano',
-  reasoning: { effort: "low" },
-  text: { verbosity: "medium" },
-  input: [
-    {
-      role: 'user',
-      content: source_prompt
-    },
-    {
-      role: 'assistant',
-      content: source_response.output_text
-    },
-    {
-      role: 'user',
-      content: package_prompt
-    },
-    {
-      role: 'assistant',
-      content: package_response.output_text
-    },
-    {
-      role: 'user',
-      content: changelog_prompt
-    }
-  ]
-})
-
-await changelog.send({
-  content: changelog_response.output_text,
-  allowedMentions: {
-    parse: []
+  reasoning: {
+    effort: 'high'
+  },
+  text: {
+    verbosity: 'high',
+    format: zodTextFormat(z.object({
+      modifyJobs: z.array(z.object({
+        file: z.string(),
+        type: z.enum(['append', 'replace', 'delete']),
+        source: z.string(),
+        destination: z.string()
+      })),
+      commitMessage: z.string(),
+      changelog: z.string()
+    }), "output")
   }
 })
 
-const commit_response = await client.responses.create({
-  model: 'gpt-5-nano',
-  reasoning: { effort: "low" },
-  text: { verbosity: "low" },
-  input: [
-    {
-      role: 'user',
-      content: source_prompt
-    },
-    {
-      role: 'assistant',
-      content: source_response.output_text
-    },
-    {
-      role: 'user',
-      content: package_prompt
-    },
-    {
-      role: 'assistant',
-      content: package_response.output_text
-    },
-    {
-      role: 'user',
-      content: changelog_prompt
-    },
-    {
-      role: 'assistant',
-      content: changelog_response.output_text
-    },
-    {
-      role: 'user',
-      content: commit_prompt
-    }
-  ]
-})
+await changelog.send(new Date() + ' finished: ' + source_response.output_text.length + ' characters')
+await changelog.send(response.output_parsed.changelog)
+
+for (const job of response.output_parsed.modifyJobs) {
+  const file = sourceFileContents.find((v) => v.fileName === job.file)
+  if (!file) {
+    console.error('file not found', job.file)
+    continue
+  }
+
+  if (job.type === 'append') {
+    file.data = file.data.split('\n').slice(0, parseInt(job.source)-1).join('\n') + '\n' + job.destination + '\n' + file.data.split('\n').slice(parseInt(job.source)-1).join('\n')
+  }
+  
+  if (job.type === 'replace') {
+    file.data = file.data.replaceAll(job.source, job.destination)
+  }
+  
+  if (job.type === 'delete') {
+    file.data = file.data.split('\n').filter((v, i) => i !== parseInt(job.source)-1).join('\n')
+  }
+
+  await fs.writeFile('./workspace/' + job.file, file.data, 'utf-8')
+}
+
+console.log('Files written, committing: ' + response.output_parsed.commitMessage)
+console.log('Changelog:\n' + response.output_parsed.changelog)
 
 fs.appendFileSync(process.env.GITHUB_OUTPUT, 'COMMIT_MESSAGE='+commit_response.output_text+'\n')
 process.exit()
